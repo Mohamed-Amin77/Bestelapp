fd156cb602a76faad78cc0be71824cac70147eb5
Linabenhaj <Benhaj.L@outlook.com>
2025-06-15 12:04:23 +0200

    Dashboard magazijnier aangemaakt en bijhorende controller

---
+++ src/main/java/com/ehb/bestelapp/controller/MagazijnierController.java
@@ -0,0 +1,17 @@
+package com.ehb.bestelapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/magazijnier")
+public class MagazijnierController {
+
+    // Dashboard-pagina
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "magazijnier/dashboard";
+    }
+
+}
